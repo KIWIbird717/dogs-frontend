5662c3d5809076d9b921804917c804f4923f186e
shuverida <bashtarov@outlook.com>
2024-07-31 18:24:12 +0300

    Added the page stats

--- app/stats/page.tsx
+++ app/stats/page.tsx
@@ -8,29 +8,27 @@ import { StatsMain } from "@/widgets/StatsMain";
 import Gradient1 from "@/public/images/svg/leaderboard/gradient/gradient1.svg";
 import Gradient2 from "@/public/images/svg/leaderboard/gradient/gradient2.svg";
 
-interface IStatsProps {
-}
+interface IStatsProps {}
 
 export interface IRank {
-  rank: string,
-  value: string
-  description: string
-  image: any,
-  users: IUserPlayer[]
+  rank: string;
+  value: string;
+  description: string;
+  image: any;
+  users: IUserPlayer[];
 }
 
 const Stats: NextPage<IStatsProps> = () => {
-
   return (
     <View
       fadeInOnLoad
-      className="relative flex h-screen w-full flex-col gap-4 !overflow-hidden px-4 pt-6 "
+      className="relative flex h-screen w-full flex-col gap-4 !overflow-hidden px-4 pt-6"
     >
       <Header />
       <StatsMain />
 
       <Gradient1 className={"absolute left-0 top-0 z-[1]"} />
-      <Gradient2 className={"absolute right-0 bottom-0 z-[1]"} />
+      <Gradient2 className={"absolute bottom-0 right-0 z-[1]"} />
       <Navbar />
     </View>
   );
