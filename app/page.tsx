d9eb463b3da5554fb2f536a69976301cf29abc1e
Evgeniy Trubnikov <parmesanshees@mail.ru>
2024-08-07 17:24:30 +0300

    start connection api

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-import { View } from "@/shared/layout/View";
 import { redirect } from "next/navigation";
 
 export default function Home() {
   redirect("/loading");
-  return (
-    <View fadeInOnLoad className="flex w-full items-center justify-center px-16">
-      <h1>Ready to develop Telegram webapp</h1>
-    </View>
-  );
 }
