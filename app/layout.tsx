95dd560c186af8235a7460d3eacc170154100ca2
shuverida <bashtarov@outlook.com>
2024-09-01 19:28:28 +0300

    added infinity scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,25 +6,28 @@ import { Epilogue } from "next/font/google";
 import "@/public/styles/globals.scss";
 import Script from "next/script";
 import StoreProvider from "@/shared/lib/redux-store/StoreProvider";
+import TanStackQueryProvider from "@/shared/providers/TanStackQueryProvider";
 
 const epilogue = Epilogue({ subsets: ["latin"] });
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <head>
-        <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
-      </head>
-      <body className={epilogue.className}>
-        <StoreProvider>
-          <ModalProvider />
-          {children}
-        </StoreProvider>
-      </body>
+    <head>
+      <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
+    </head>
+    <body className={epilogue.className}>
+    <TanStackQueryProvider>
+      <StoreProvider>
+        <ModalProvider />
+        {children}
+      </StoreProvider>
+    </TanStackQueryProvider>
+    </body>
     </html>
   );
 }
