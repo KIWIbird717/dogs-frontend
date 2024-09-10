a0f8f56a4283cce978ef23669a1d33ec20531a73
Evgeniy Trubnikov <parmesanshees@mail.ru>
2024-09-11 01:06:39 +0300

    update button

--- widgets/MainForClicker/shared/ui/Clicker.tsx
+++ widgets/MainForClicker/shared/ui/Clicker.tsx
@@ -20,7 +20,7 @@ export const Clicker: FC<IClickerProps> = ({ handleClick, level }) => {
         onTouchStart={() => setIsTaped(true)}
         onTouchEnd={handleButtonClick}
         className={
-          "max-h-[296px] w-[270px] max-w-[296px] rounded-[52px] bg-gradient-button-sec p-4 shadow-buttonSec transition-all duration-100"
+          "max-h-[296px] w-[270px] max-w-[296px] rounded-[52px] bg-[rgba(255,255,255,0.01)] p-4 backdrop-blur-lg transition-all duration-100"
         }
         style={{ transform: isTaped ? "scale(0.9)" : "scale(1)" }}
       >
