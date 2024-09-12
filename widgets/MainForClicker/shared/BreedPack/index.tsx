bac91814a57825d8cb7bd5055e1d64103a7faa52
Evgeniy Trubnikov <parmesanshees@mail.ru>
2024-09-12 20:38:20 +0300

    fix: pack

--- widgets/MainForClicker/shared/BreedPack/index.tsx
+++ widgets/MainForClicker/shared/BreedPack/index.tsx
@@ -11,7 +11,7 @@ import { cn } from "@/shared/lib/utils/cn";
 const MotionButton = dynamic(() => import("framer-motion").then((mod) => mod.motion.button));
 
 const TOTAL_PRIZES = 7;
-const DAY_IN_MS = 60 * 1000; // 24 часа в миллисекундах
+const DAY_IN_MS = 24 * 60 * 60 * 1000; // 24 часа в миллисекундах
 const MIN_INTERVAL = 1 * 60 * 60 * 1000; // минимальный интервал в 1 час
 const MAX_INTERVAL = 2 * 60 * 60 * 1000; // максимальный интервал в 2 часа
 
